f5efbdf046946ac866448b756c03256b8799a01f
dooplu <43157907+dooplu@users.noreply.github.com>
2023-11-12 12:03:39 -0500

    created skeletons for important features

---
+++ ReportEntry.py
@@ -0,0 +1,10 @@
+class ReportEntry:
+    def __init__(self, uname, caption, desc, why):
+        self.uname = uname
+        self.caption = caption
+        self.desc = desc
+        self.why = why
+
+    
+    def returnHTML():
+        pass
