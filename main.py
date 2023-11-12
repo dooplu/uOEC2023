11f609e8f7d6fdc9d316c8943bcfdb667ae74347
dooplu <43157907+dooplu@users.noreply.github.com>
2023-11-12 13:12:13 -0500

    able to retrieve admin and report pages + auto fill report info

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World ITS IDIR!</p>"
+
+@app.route("/reports", methods=['GET', 'POST'])
+@app.route("/reports/<link>+<username>")
+def reports(link="", username=""):
+    if request.method == 'GET':
+        return render_template("reports.html", link=link, username=username)
+    else: 
+        pass
+
+@app.route("/admin")
+def admin():
+    return "<h1> admin </h1>"
